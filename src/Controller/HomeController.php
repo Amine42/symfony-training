8c9d410bdfc9d2c5f050477dd307255fde32532e
Amine42 <amine.bouraghda@gmail.com>
2020-05-14 07:58:11 +0200

    add multiple route for index page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route({"/", "/home",}, name="home")
      */
     public function index()
     {
@@ -16,4 +16,14 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    // /**
+    //  * @Route("home", name="home")
+    //  */
+    // public function home()
+    // {
+    //     return $this->render('home/index.html.twig', [
+    //         'controller_name' => 'HomeController',
+    //     ]);
+    // }
 }
